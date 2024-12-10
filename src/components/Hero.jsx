9ee567fef992bdfdd12c4982a2bd43aa2db0c714
Sanusi15 <websan1512@gmail.com>
2024-12-10 11:41:16 +0700

    Forward commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
               <span className="relative w-2 h-2 rounded-full bg-emerald-400">
                 <span className="absolute inset-0 w-2 h-2 rounded-full bg-emerald-400 animate-ping"></span>
               </span>
-              Bersedia for work
+              Available for work
             </div>
           </div>
           <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
